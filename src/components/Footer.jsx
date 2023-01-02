5dee2f421bd8541148251d2787a00502b6ec5103
Lex Mullin <alemulli@gmail.com>
2023-01-02 15:40:36 -0500

    css editing

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,30 +1,44 @@
 import React from "react";
 
 const Footer = () => {
+  function copyText() {
+    /* Copy text into clipboard */
+    navigator.clipboard.writeText("alemulli@gmail.com");
+    document.getElementsByClassName("copied")[0].style.display = "inline";
+    setTimeout(function () {
+      document.getElementsByClassName("copied")[0].style.display = "none";
+    }, 3000);
+  }
+
   return (
     <div id="footer">
       <div className="socials">
-              <a
-        href="https://www.linkedin.com/in/alexa-mullin-14740817a/"
-        target="_blank"
-        ><img
-          src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/81/LinkedIn_icon.svg/2048px-LinkedIn_icon.svg.png"
-          alt="LinkedIn Logo"
-          className="socialmedia"
-      /></a>
-      <a href="https://github.com/alemulli" target="_blank"
-        ><img
-          src="https://upload.wikimedia.org/wikipedia/commons/9/91/Octicons-mark-github.svg"
-          alt="Github Logo"
-          className="socialmedia"
-      /></a>
-      </div>  
+        <a
+          href="https://www.linkedin.com/in/alexa-mullin-14740817a/"
+          target="_blank"
+        >
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/81/LinkedIn_icon.svg/2048px-LinkedIn_icon.svg.png"
+            alt="LinkedIn Logo"
+            className="socialmedia"
+          />
+        </a>
+        <a href="https://github.com/alemulli" target="_blank">
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/9/91/Octicons-mark-github.svg"
+            alt="Github Logo"
+            className="socialmedia"
+          />
+        </a>
+      </div>
       <div className="contact">
         <strong>Contact me:</strong>
-        <p>alemulli@gmail.com</p>
+        <p id="email" onClick={copyText}>
+          alemulli@gmail.com{" "}
+        </p>
+        <span className="copied">Copied to Clipboard</span>
       </div>
-      {/* can I add the ability to copy my email to clipboard on click? */}
-  </div>
+    </div>
   );
 };
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,20 +3,41 @@ import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
 
-  async function changeActive () {
-    //function that adds className active to current tab, will probably need 3 states (one for each tab) that can be toggled between active and null//
-    //may need 3 functions//
-    console.log("tabchanged")
+  async function removeActiveClass () {
+    let nav1 = document.getElementsByClassName("navlink")[0]
+    let nav2 = document.getElementsByClassName("navlink")[1]
+    let nav3 = document.getElementsByClassName("navlink")[2]
+    
+    nav1.classList.remove("active")
+    nav2.classList.remove("active")
+    nav3.classList.remove("active")
   }
 
+  async function changeActivetoHome () {
+    removeActiveClass()
+    let homeTab = document.getElementById("homeNav")
+    homeTab.classList.add("active")
+  }
+
+  async function changeActivetoAbout () {
+    removeActiveClass()
+    let aboutTab = document.getElementById("aboutNav")
+    aboutTab.classList.add("active")
+  }
+
+  async function changeActivetoProjects () {
+    removeActiveClass()
+    let projectsTab = document.getElementById("projectsNav")
+    aboutTab.classList.add("active")
+  }
 
   return (
     <div className="header">
         <h1 className="headerh1">Lex Mullin</h1>
         <nav className="navbar">
-          <NavLink to ="/" alt="Home Page" className="navlink" onClick={changeActive}>Home</NavLink>
-          <NavLink to ="/aboutme" alt="About Me" className="navlink" onClick={changeActive}>About Me</NavLink>
-          <NavLink to ="/portfolio" alt="Portfolio" className="navlink" onClick={changeActive}>Portfolio</NavLink>
+          <NavLink to ="/" alt="Home Page" id="homeNav" className="navlink" onClick={changeActivetoHome}>Home</NavLink>
+          <NavLink to ="/aboutme" alt="About Me" id="aboutNav" className="navlink" onClick={changeActivetoAbout}>About Me</NavLink>
+          <NavLink to ="/portfolio" alt="Projects" id="projectsNav" className="navlink" onClick={changeActivetoProjects}>Projects</NavLink>
       </nav>
   </div>
   );
