25be110f3efc877661048e92306006a239a41891
Lex Mullin <alemulli@gmail.com>
2023-01-02 15:59:18 -0500

    Some work on HomePage

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -7,25 +7,21 @@ const HomePage = () => {
       <br />
       <h1 className="mainh1">Hi, my name is Lex Mullin.</h1>
       <p>
-        I am a career switcher from education to web development. When I was in
-        the classroom, I often found myself thinking 'I wish XYZ existed on the
-        internet.' And so, I decided to hone the ability to make XYZ exist. I
-        want to make tools that help make teachers lives easier which will in
-        turn help educate students more efficiently. I want to make engaging and
-        educational learning experiences online that can be used by students. I
-        intend to use my experience to draw ideas for development and personal
-        projects, but would also like continued communication and feedback from
-        teachers still in the classroom.
+      I am a former teacher who decided to learn coding. Learning is what I am most passionate about in my life, and I full-heartedly believe that I made the best choice when picking coding as my next thing to learn. I instantly fell in love with the idea of working in programming, as it is an ever evolving field where there is constantly more to learn and new ways to grow. 
+      </p><p>
+      I have recently graduated from the Virginia Tech Coding Bootcamp powered by Fullstack Academy. While I still fully intend to continue learning independently, I am now looking for my place in the field. My goal is to utilize my newfound programming skills while also leveraging the skills I developed in the multifaceted and demanding education environment.
       </p>
-      <h3>My technologies:</h3>
-      <ul>
-        <li>HTML</li>
-        <li>CSS</li>
+      <h3>My skills:</h3>
+      <ul id="skills">
+        <li>HTML5</li>
+        <li>CSS3</li>
         <li>JavaScript</li>
-        <li>React</li>
+        <li>React.js</li>
         <li>Node.js</li>
-        <li>SQL</li>
-        <li>Python</li>
+        <li>Express.js</li>
+        <li>PostgreSQL</li>
+        <li>Github</li>
+        {/* <li>Python</li> */}
       </ul>
       <img src="https://ghchart.rshah.org/5ec8b9/alemulli" alt="2016rshah's Blue Github Chart" /> 
         {/* can change the color by putting a new hex code in place of 409ba5 */}
