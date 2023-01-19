e69a704c902b966e6997730ee8c65e5a9e1bc4ba
Lex Mullin <alemulli@gmail.com>
2023-01-19 12:50:44 -0500

    projects

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -8,15 +8,24 @@ const Portfolio = () => {
         <h1 className="mainh1">My projects.</h1>
         <div className="section1">
           <h3>GraceShopper — Hot Cars</h3>
-          <iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/eEYeB9Y2bEc?" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
+          <iframe
+            width="560"
+            height="315"
+            src="https://www.youtube-nocookie.com/embed/eEYeB9Y2bEc?"
+            title="YouTube video player"
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            allowFullScreen
+          ></iframe>
           <p>
             <a href="https://hot-cars.onrender.com/" target="_blank">
               GraceShopper — Hot Cars
             </a>{" "}
             is an e-commerce website for buying and selling used cars, developed
-            as the capstone project for Virginia Tech Coding Bootcamp powered by FullStack Academy. I contributed as
-            a fullstack developer for this project alongside teammates Leo Cruz,
-            Alex Winston, Dillan May, and Irfan Pekusic.
+            as the capstone project for Virginia Tech Coding Bootcamp powered by
+            FullStack Academy. I contributed as a fullstack developer for this
+            project alongside teammates Leo Cruz, Alex Winston, Dillan May, and
+            Irfan Pekusic.
           </p>
           <p>
             This website was developed using PostgreSQL, Express.js, and
@@ -33,27 +42,32 @@ const Portfolio = () => {
             </li>
             <li>
               {" "}
-              Authentication was set up via JWT using BCrypt to hash passwords for
-              extra security.
+              Authentication was set up via JWT using BCrypt to hash passwords
+              for extra security.
             </li>
             <li>
-              We implemented a functional guest user interface that can be used to
-              add and remove items from the cart if the user is not logged in or
-              doesn't have a registered account.
+              We implemented a functional guest user interface that can be used
+              to add and remove items from the cart if the user is not logged in
+              or doesn't have a registered account.
             </li>
             <li>Media queries were used for responsive web design. </li>
             <li>
-              <span>Github Repositories: <a
+              <span>
+                Github Repositories:{" "}
+                <a
                   href="https://github.com/TeamDallasVirginiaChicagoFlorida/FrontEnd"
                   target="_blank"
                 >
                   Front-end
-                </a>, <a
+                </a>
+                ,{" "}
+                <a
                   href="https://github.com/TeamDallasVirginiaChicagoFlorida/BackEnd"
                   target="_blank"
                 >
                   Back-end
-                </a></span>
+                </a>
+              </span>
             </li>
           </ul>
           {/* <img
@@ -63,23 +77,109 @@ const Portfolio = () => {
           /> */}
         </div>
         <div className="section2">
-          <h3>Project 2: Title</h3>
-          <p>details, details, details</p>
+          <h3>Fitness Trackr</h3>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc tempus
-            laoreet maximus. Etiam non sem et purus pulvinar ultrices vitae vel
-            risus. Suspendisse ut tellus eu purus posuere bibendum. Nam
-            tristique, nibh in tempor ultrices, nisl neque tempus eros, eu
-            convallis nisl dolor in lacus. Vivamus ornare sagittis varius. Morbi
-            in diam et nisl egestas fermentum. Quisque convallis feugiat nisl,
-            ut elementum sem imperdiet at. In condimentum libero non metus
-            fringilla, pharetra vehicula felis consectetur. Cras ornare
-            condimentum pharetra. Aenean luctus metus sit amet turpis luctus
-            dictum. Pellentesque porta sit amet turpis quis convallis.
-            Pellentesque suscipit lacus vitae commodo dapibus. Aenean rutrum
-            blandit ante vel viverra. Mauris aliquam ante velit, vel suscipit
-            velit cursus id.
+            Fitness Trackr is a MyFitnessPal-esque workout routine sharing
+            website, developed while attending Virginia Tech Coding Bootcamp
+            powered by FullStack Academy. I contributed as a fullstack developer
+            for this project alongside my teammate Charles Valdez.
           </p>
+          <p>
+            This website was developed using PostgreSQL, Express.js, React.js,
+            and Three.js.
+          </p>
+          <ul>
+            {" "}
+            <li>
+              {" "}
+              We designed the back-end database, queries, and API routes using
+              test driven development.
+            </li>
+            <li>
+              {" "}
+              We designed a responsive front-end, aiming for a sleek and modern
+              design for our webpage.
+            </li>
+            <li>
+              {" "}
+              Authentication was set up via JWT using BCrypt to hash passwords
+              for extra security.
+            </li>
+            <li>
+              Personal contributions included implementing the modal pop-up
+              menus and helping reinforce my team's understanding of front-end
+              development, making API fetches and using React, as well as using
+              Three.js to design and implement an animated logo for the website.
+            </li>
+            <li>
+              <span>
+                Github Repositories:{" "}
+                <a
+                  href="https://github.com/alemulli/fitnessFront"
+                  target="_blank"
+                >
+                  Front-end
+                </a>
+                ,{" "}
+                <a
+                  href="https://github.com/alemulli/fitnesstrackr"
+                  target="_blank"
+                >
+                  Back-end
+                </a>
+              </span>
+            </li>
+          </ul>
+          <img
+            src="https://placeimg.com/1000/500/tech?2"
+            className="screenshots"
+            alt="Screenshot of Project"
+          />
+        </div>
+        <div className="section2">
+          <h3>JuiceBox</h3>
+          <p>
+            JuiceBox is a public message board web app, developed while
+            attending Virginia Tech Coding Bootcamp powered by FullStack
+            Academy. I contributed as a fullstack developer for this project
+            alongside my teammate Timothy Brennan.
+          </p>
+          <p>
+            This website was developed using PostgreSQL, Express.js, and
+            React.js.
+          </p>
+          <ul>
+            {" "}
+            <li>
+              {" "}
+              We designed and implemented the front-end website to what was
+              initially a back-end only project.
+            </li>
+            <li> Authentication was set up via JWT.</li>
+            <li>
+              We experimented with different front-end design elements, such as
+              implementing modal pop-up menus and animations using CSS.
+            </li>
+            <li>
+              We are in the process of redeploying a live version of this
+              website.
+            </li>
+            <li>
+              <span>
+                Github Repositories:{" "}
+                <a
+                  href="https://github.com/alemulli/juiceboxFront"
+                  target="_blank"
+                >
+                  Front-end
+                </a>
+                ,{" "}
+                <a href="https://github.com/alemulli/juicebox" target="_blank">
+                  Back-end
+                </a>
+              </span>
+            </li>
+          </ul>
           <img
             src="https://placeimg.com/1000/500/tech?2"
             className="screenshots"
@@ -87,28 +187,37 @@ const Portfolio = () => {
           />
         </div>
         <div className="section3">
-          <h3>Project 3: Title</h3>
-          <p>details, details, details</p>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc tempus
-            laoreet maximus. Etiam non sem et purus pulvinar ultrices vitae vel
-            risus. Suspendisse ut tellus eu purus posuere bibendum. Nam
-            tristique, nibh in tempor ultrices, nisl neque tempus eros, eu
-            convallis nisl dolor in lacus. Vivamus ornare sagittis varius. Morbi
-            in diam et nisl egestas fermentum. Quisque convallis feugiat nisl,
-            ut elementum sem imperdiet at. In condimentum libero non metus
-            fringilla, pharetra vehicula felis consectetur. Cras ornare
-            condimentum pharetra. Aenean luctus metus sit amet turpis luctus
-            dictum. Pellentesque porta sit amet turpis quis convallis.
-            Pellentesque suscipit lacus vitae commodo dapibus. Aenean rutrum
-            blandit ante vel viverra. Mauris aliquam ante velit, vel suscipit
-            velit cursus id.
-          </p>
+          <h3>Snake</h3>
           <img
-            src="https://placeimg.com/1000/500/tech?3"
+            src="https://res.cloudinary.com/dyqjp130l/image/upload/v1673985159/Tech/snake_omkjst.png"
             className="screenshots"
             alt="Screenshot of Project"
           />
+          <p>
+            <a href="https://lexsnake.netlify.app/" target="_blank">
+              Snake
+            </a>{" "}
+            is a front-end vanilla JavaScript webapp where you can play a
+            traditional Snake game.
+          </p>
+          <ul>
+            {" "}
+            <li> Difficulty and field size selectors are available for the game. </li>
+            <li>
+              {" "}
+              High score, average score, and records from previous matches are
+              stored for your play session.
+            </li>
+            <li>
+              Snake games are traditionally made in JavaScript using canvas, but
+              I took a different approach in using a table and having the snake navigate across the cells.{" "}
+            </li>
+            <li>
+              <a href="https://github.com/alemulli/arcade" target="_blank">
+                Github Repository
+              </a>
+            </li>
+          </ul>
         </div>
       </main>
     </div>
